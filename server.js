9bc4adcbb5e66a9c8d322d444023ca39131038bb
jedrek89 <marcinj3@gmail.com>
2022-12-08 10:23:54 +0100

    fix server.js

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.get('/*', function(req, res) {
     headers: {
         //API key created in heroku
     'api-key': process.env.OpenWeatherAPIKey, 
+    API_KEY: process.env.OpenWeatherAPIKey,
     }
     };
 
@@ -45,7 +46,7 @@ app.get('/*', function(req, res) {
   * This url will be used in the angular app to request the api call
   * but the actual api call will made here in the server and the response will be sent back to angular app
   */
-app.get("https://api.openweathermap.org/data/2.5/forecast?q=warsaw&units=metric&lang=en&&appid=", (req,res) =>{
+app.get(`https://api.openweathermap.org/data/2.5/forecast?q=warsaw&units=metric&lang=en&&appid=${options.API_KEY}`, (req,res) =>{
 axios(options).then(dataResponse =>{
 //API response
 res.json({data: dataResponse.data});
