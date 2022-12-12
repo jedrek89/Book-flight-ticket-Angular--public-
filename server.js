52e8f6b0549baf77305613b6b67228284dc408ec
jedrek89 <marcinj3@gmail.com>
2022-12-12 14:53:50 +0100

    fix server.js

--- server.js
+++ server.js
@@ -5,29 +5,42 @@ router = express.Router();
 var https = require('https');
 const app = express();
 
-
 // display values from .env file
 // console.log("weatherOptions", weatherOptions);
 
 // static preview Angular App
 app.use(express.static(__dirname + "/dist/angular"));
 
-axios.get('https://worldtimeapi.org/api/timezone/Europe/Berlin')
+// fetch data from API to console
+router.route('/api/time')
+axios(process.env.WorldTimeURL)
 .then(response => {
 console.log(response);
+return console.log("data recived");
 })
 .catch(error => {
 console.log(error);
 });
 
 
-axios.get(`${process.env.OpenWeatherURL}${process.env.OpenWeatherKEY}`)
-.then(response =>  {
-console.log(response);
-})
-.catch(error => {
-console.log(error);
-});
+// with URL
+// app.get('/api/time', (req,res) =>{
+// axios(process.env.WorldTimeURL)
+// .then(response => {
+// console.log(response);
+// })
+// .catch(error => {
+// console.log(error);
+// });
+// })
+
+// axios.get(`${process.env.OpenWeatherURL}${process.env.OpenWeatherKEY}`)
+// .then(response =>  {
+// console.log(response);
+// })
+// .catch(error => {
+// console.log(error);
+// });
 
 
 // axios.get(`${process.env.FlightDataUrl}destination=${flyToVal}&origin=${flyFromVal}&currency=${currency}&show_to_affiliates=true&depart_date=${departDate}&return_date=${returnDate}`)
