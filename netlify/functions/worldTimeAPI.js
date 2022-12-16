6906ff46fdd06b2e182cd3e215522eea242181d6
jedrek89 <marcinj3@gmail.com>
2022-12-16 12:57:57 +0100

    example fetch

--- netlify/functions/worldTimeAPI.js
+++ netlify/functions/worldTimeAPI.js
@@ -1,12 +1,12 @@
 exports.handler = async function(event, context) {
-    fetch(`${Deno.env.get(WorldTimeURL)}`)
+    fetch(`${Deno.env.get("WorldTimeURL")}`)
     .then((response) => response.json())
     .then((data) => console.log(data));
 
-    return{
-        // statusCode: 200,
-        // body: JSON.stringify({message: 'Netlify function works!!'})
-        statusCode: 200,
-        body: JSON.stringify({data})
-    }
+    // return{
+    //     // statusCode: 200,
+    //     // body: JSON.stringify({message: 'Netlify function works!!'})
+    //     statusCode: 200,
+    //     body: JSON.stringify({data})
+    // }
 }
