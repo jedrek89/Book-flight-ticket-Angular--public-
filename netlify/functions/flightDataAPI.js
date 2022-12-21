2efbcd6e1f90de7ba308d4e5a06198c712d24423
jedrek89 <marcinj3@gmail.com>
2022-12-21 12:56:10 +0100

    "backend" flightDataAPI.js code optimization

--- netlify/functions/flightDataAPI.js
+++ netlify/functions/flightDataAPI.js
@@ -11,28 +11,21 @@ options = {
 
 exports.handler = async function(event, context, callback) {
     if (typeof event.body === 'undefined') {
-            return{
-                statusCode: 200,
-                body: JSON.stringify({"response": "event.body = undefined"}) 
-                }
+        return{
+            statusCode: 200,
+            body: JSON.stringify({"response": "event.body = undefined"}) 
+        }
     }
 
     // if body from POST method exist
     else{
         let eventBody = JSON.parse(event.body); // convert JSON fromat to object
-        let flyFrom = eventBody.flyFrom;
-        let departureDate = eventBody.departureDate;
-        let passNum = eventBody.passNum;
-        let flyTo = eventBody.flyTo;
-        let returnDate = eventBody.returnDate;
-        let currency = eventBody.currency;
-
         // API call
-        let response = await fetch(`https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v2/prices/week-matrix?destination=${flyTo}&origin=${flyFrom}&currency=${currency}&show_to_affiliates=true&depart_date=${departureDate}&return_date=${returnDate}`, options);
+        let response = await fetch(`https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v2/prices/week-matrix?destination=${eventBody.flyTo}&origin=${eventBody.flyFrom}&currency=${eventBody.currency}&show_to_affiliates=true&depart_date=${eventBody.departureDate}&return_date=${eventBody.returnDate}`, options);
         response = await response.json()
         return{
             statusCode: 200,
             body: JSON.stringify(response)
-            }
+        }
     }
 }
