56d8ede9b8338525d3121eb5043456788ee062c4
jedrek89 <marcinj3@gmail.com>
2022-12-28 13:00:53 +0100

    Add seats reservation - static v 0.0

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -32,13 +32,17 @@ export class SearchResultsComponent implements OnInit {
       this.sizeOfFlightData = Object.keys(this.flightData.data).length;
       this.showItemBoxC3Status.length = this.sizeOfFlightData;
       this.showItemBoxC3Status.fill(0);
-      console.log("this.showItemBoxC3Status", this.showItemBoxC3Status);
-      console.log("this.showItemBoxC3Status.length", this.showItemBoxC3Status.length);
+      // console.log("this.showItemBoxC3Status", this.showItemBoxC3Status);
+      // console.log("this.showItemBoxC3Status.length", this.showItemBoxC3Status.length);
     }
-    console.log('search results init');
-    console.log('flightData', this.flightData);
-    console.log('flight param: ', flightParam);
-    console.log("flightParameters", this.flightParameters.flyFromName);
+    // console.log('search results init');
+    // console.log('flightData', this.flightData);
+    // console.log('flight param: ', flightParam);
+    // console.log("flightParameters", this.flightParameters.flyFromName);
+    // console.log("this.seatsA", this.seatsA);
+
+    // Call seats resevation
+    this.seatsArrInit();
   }
 
   showItemBoxC3(data: any){
@@ -46,9 +50,36 @@ export class SearchResultsComponent implements OnInit {
     (this.showItemBoxC3Status[data] === 1) ? this.showItemBoxC3Status[data] = 0 : this.showItemBoxC3Status[data] = 1;
   }
 
-  seatsReservation(data: string){
-    console.log("seatsReservation", data);
-  console.log("seatsA", this.seatsA);
+  // Seats reservation
+  seatsReservation(data1: string, data2 : number){
+    (data1 === 'A' && this.seatsA[data2] === 0) ? this.seatsA[data2] = 1 : this.seatsA[data2] = 0;
+    console.log(`seats${data1}`+[data2]);
+    (data1 === 'B' && this.seatsB[data2] === 0) ? this.seatsB[data2] = 1 : this.seatsB[data2] = 0;
+    console.log(`seats${data1}`+[data2]);
+    (data1 === 'C' && this.seatsC[data2] === 0) ? this.seatsC[data2] = 1 : this.seatsC[data2] = 0;
+    console.log(`seats${data1}`+[data2]);
+    (data1 === 'D' && this.seatsD[data2] === 0) ? this.seatsD[data2] = 1 : this.seatsD[data2] = 0;
+    console.log(`seats${data1}`+[data2]);
+    (data1 === 'E' && this.seatsE[data2] === 0) ? this.seatsE[data2] = 1 : this.seatsE[data2] = 0;
+    console.log(`seats${data1}`+[data2]);
+    (data1 === 'F' && this.seatsF[data2] === 0) ? this.seatsF[data2] = 1 : this.seatsF[data2] = 0;
+    console.log(`seats${data1}`+[data2]);
+  }
+
+  // Seats arr init
+  seatsArrInit(){
+    this.seatsA.length = 38;
+    this.seatsA.fill(0);
+    this.seatsB.length = 38;
+    this.seatsB.fill(0);
+    this.seatsC.length = 38;
+    this.seatsC.fill(0);
+    this.seatsD.length = 38;
+    this.seatsD.fill(0);
+    this.seatsE.length = 38;
+    this.seatsE.fill(0);
+    this.seatsF.length = 38;
+    this.seatsF.fill(0);
   }
 
 }
